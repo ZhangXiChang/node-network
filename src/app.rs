e2bd1d8f3a7710d3d5fd5b655bf3eb7f69bc5c1b
ZhangXiChang <yxrbeigua@outlook.com>
2024-06-20 09:14:05 +0800

    基本

--- src/app.rs
+++ src/app.rs
@@ -1,23 +1,28 @@
 use yew::prelude::*;
 
+use crate::invoke;
+
 pub struct App {}
 impl yew::Component for App {
     type Message = ();
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Self {}
     }
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html!(
             <>
-            <button class="bubbly-button">{"Click me!"}</button>
+            <button class="bubbly-button">{"点我吧！"}</button>
             </>
         )
     }
-    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
         if first_render {
-            js_sys::eval(include_str!("../script.js")).unwrap();
+            match js_sys::eval(include_str!("../script.js")) {
+                Ok(_) => (),
+                Err(_) => invoke::exit(),
+            }
         }
     }
 }
--- src/invoke.rs
+++ src/invoke.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-    fn invoke(cmd: &str) -> JsValue;
+    fn invoke(cmd: &str);
 }
 
 pub fn exit() {
