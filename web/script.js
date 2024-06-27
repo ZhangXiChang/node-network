98f723f2b1f38717b767be8c0327e6a652f00e0f
ZhangXiChang <yxrbeigua@outlook.com>
2024-06-27 10:48:28 +0800

    禁止选中内容

--- web/script.js
+++ web/script.js
@@ -1,3 +1,4 @@
-window.addEventListener("contextmenu", (e) => e.preventDefault());
+window.addEventListener("contextmenu", (e) => e.preventDefault())
+window.addEventListener("selectstart", (e) => e.preventDefault())
 
 document.title = "节点网络"
