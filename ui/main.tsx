39dd5f8d0d5cce98277dc9bf03f2f1a04df64962
ZhangXiChang <yxrbeigua@outlook.com>
2024-12-01 08:10:09 +0800

    更新

--- ui/main.tsx
+++ ui/main.tsx
@@ -3,7 +3,4 @@ import "virtual:uno.css";
 import "./style.css";
 import Viewport from "./viewport/mod";
 
-window.addEventListener("contextmenu", (e) => e.preventDefault());
-window.addEventListener("keydown", (e) => e.key != "F12" ? e.preventDefault() : null);
-
 render(() => <Viewport />, document.body);
