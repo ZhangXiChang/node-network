e61e0c7d2ee0837e675b908a4bdee7141e7a9fe7
ZhangXiChang <yxrbeigua@outlook.com>
2025-01-06 18:30:19 +0800

    空ark-ui

--- ui/root/index.tsx
+++ ui/root/index.tsx
@@ -1,24 +1,5 @@
-import { Checkbox } from "@ark-ui/solid";
-import { CheckIcon } from "lucide-solid";
-
-const items = [
-    { label: "React", value: "react" },
-    { label: "Solid", value: "solid" },
-    { label: "Vue", value: "vue" },
-];
-
 export function Root() {
-    return <Checkbox.Group defaultValue={["react"]} onValueChange={console.log}>
-        {items.map((item) => (
-            <Checkbox.Root value={item.value}>
-                <Checkbox.Label>{item.label}</Checkbox.Label>
-                <Checkbox.Control>
-                    <Checkbox.Indicator>
-                        <CheckIcon />
-                    </Checkbox.Indicator>
-                </Checkbox.Control>
-                <Checkbox.HiddenInput />
-            </Checkbox.Root>
-        ))}
-    </Checkbox.Group>;
+    return (
+        <></>
+    );
 }
