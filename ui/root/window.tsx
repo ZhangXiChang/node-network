ca70fdd9014efe019b5ad99e094e01fcbb3ef5d3
ZhangXiChang <yxrbeigua@outlook.com>
2025-01-07 16:18:32 +0800

    窗口框架

--- ui/root/window.tsx
+++ ui/root/window.tsx
@@ -19,24 +19,24 @@ export function Window(props: { children?: JSX.Element }) {
     return (
         <div class="absolute size-full flex flex-col bg-white">
             <div data-tauri-drag-region class="h-32px flex items-center">
-                <div class="w-32px flex justify-center">
+                <div data-tauri-drag-region class="w-32px flex justify-center">
                     <div class="rounded hover:cursor-pointer hover:bg-gray-3" onclick={() => openUrl("https://github.com/ZhangXiChang/node-network")}>
                         <div class="i-line-md:github-loop h-16px w-16px" />
                     </div>
                 </div>
-                <label class="h-24px font-bold" style={{ "text-shadow": "0px 0px 10px gray" }}>节点网络</label>
-                <div class="flex flex-auto" />
-                <div class="w-32px flex justify-center">
+                <label data-tauri-drag-region class="h-24px font-bold" style={{ "text-shadow": "0px 0px 10px gray" }}>节点网络</label>
+                <div data-tauri-drag-region class="flex flex-auto" />
+                <div data-tauri-drag-region class="w-32px flex justify-center">
                     <div class="rounded hover:cursor-pointer hover:bg-gray-3" onclick={mainWindow.minimize}>
                         <div class="i-mdi:window-minimize h-16px w-16px" />
                     </div>
                 </div>
-                <div class="w-32px flex justify-center">
+                <div data-tauri-drag-region class="w-32px flex justify-center">
                     <div class="rounded hover:cursor-pointer hover:bg-gray-3" onclick={mainWindow.toggleMaximize}>
                         <div class={windowToggleMaximizeIcon()} />
                     </div>
                 </div>
-                <div class="w-32px flex justify-center">
+                <div data-tauri-drag-region class="w-32px flex justify-center">
                     <div class="rounded hover:cursor-pointer hover:bg-gray-3" onclick={mainWindow.close}>
                         <div class="i-mdi:window-close h-16px w-16px" />
                     </div>
